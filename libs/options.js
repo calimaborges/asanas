e2bb4e33075cdee9bf039465b6634ad804655ee1
Carlos Augusto Borges <calimaborges@gmail.com>
2021-01-14 04:34:21 -0300

    Melhoria de nome das opções

--- libs/options.js
+++ libs/options.js
@@ -1,29 +1,29 @@
 export const planos = {
-  "plano-alto": { description: "plano alto", type: "plano" },
-  "plano-medio": { description: "plano médio", type: "plano" },
-  "plano-baixo": { description: "plano baixo", type: "plano" },
-  invertida: { description: "invertida", type: "plano" },
+  "plano-alto": { description: "Plano Alto", type: "plano" },
+  "plano-medio": { description: "Plano Médio", type: "plano" },
+  "plano-baixo": { description: "Plano Baixo", type: "plano" },
+  invertida: { description: "Invertida", type: "plano" },
 };
 
 export const equilibrios = {
-  equilibrio: { description: "equilíbrio", type: "equilibrio" },
-  tracao: { description: "tração", type: "equilibrio" },
-  flexao: { description: "flexão", type: "equilibrio" },
-  extensao: { description: "extensão", type: "equilibrio" },
-  "flexao-lateral": { description: "flexão lateral", type: "equilibrio" },
-  torcao: { description: "torção", type: "equilibrio" },
+  equilibrio: { description: "Equilíbrio", type: "equilibrio" },
+  tracao: { description: "Tração", type: "equilibrio" },
+  flexao: { description: "Flexão", type: "equilibrio" },
+  extensao: { description: "Extensão", type: "equilibrio" },
+  "flexao-lateral": { description: "Flexão Lateral", type: "equilibrio" },
+  torcao: { description: "Torção", type: "equilibrio" },
 };
 
 export const alongamentos = {
-  "alongamento-peitoral": { description: "peitoral", type: "alongamento" },
-  "alongamento-costas": { description: "costas", type: "alongamento" },
-  "alongamento-abdomen": { description: "abdômen", type: "alongamento" },
-  "alongamento-pernas": { description: "pernas", type: "alongamento" },
+  "alongamento-peitoral": { description: "Peitoral", type: "alongamento" },
+  "alongamento-costas": { description: "Costas", type: "alongamento" },
+  "alongamento-abdomen": { description: "Abdômen", type: "alongamento" },
+  "alongamento-pernas": { description: "Pernas", type: "alongamento" },
 }
 
 export const fortalecimentos = {
-  "fortalecimento-peitoral": { description: "peitoral", type: "fortalecimento" },
-  "fortalecimento-costas": { description: "costas", type: "fortalecimento" },
-  "fortalecimento-abdomen": { description: "abdômen", type: "fortalecimento" },
-  "fortalecimento-pernas": { description: "pernas", type: "fortalecimento" },
+  "fortalecimento-peitoral": { description: "Peitoral", type: "fortalecimento" },
+  "fortalecimento-costas": { description: "Costas", type: "fortalecimento" },
+  "fortalecimento-abdomen": { description: "Abdômen", type: "fortalecimento" },
+  "fortalecimento-pernas": { description: "Pernas", type: "fortalecimento" },
 }
